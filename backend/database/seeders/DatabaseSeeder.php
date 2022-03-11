f27f183336889ba87142020b3d84ef082c95a79e
Wellisson Ribeiro <welleh10@gmail.com>
2022-03-11 09:59:13 -0300

    :hammer: removed backend project made with Lumen, and added Laravel

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,12 +8,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        // \App\Models\User::factory(10)->create();
     }
 }
